7a1cf4f966f70bc146b17b9de765a5c07db2853f
Meddhavi <meddhavisatija@gmail.com>
2020-06-17 17:53:06 +0530

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import Reservation from "./ReservationComponent";
+import Favorites from "./FavoriteComponent";
 import Dishdetail from "./DishDetailComponent";
 import {
   View,
@@ -189,6 +190,31 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: { screen: Favorites },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerTintColor: "#fff",
+      headerLeft: (
+        <Icon
+          name="menu"
+          size={24}
+          iconStyle={{ color: "white" }}
+          onPress={() => navigation.navigate("DrawerToggle")}
+        />
+      ),
+    }),
+  }
+);
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView
@@ -260,6 +286,21 @@ const MainNavigator = createDrawerNavigator(
             type="font-awesome"
             size={22}
             color={tintColor}
+          />
+        ),
+      },
+    },
+    Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        title: "My Favorites",
+        drawerLabel: "My Favorites",
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name="heart"
+            type="font-awesome"
+            size={24}
+            iconStyle={{ color: tintColor }}
           />
         ),
       },
--- redux/comments.js
+++ redux/comments.js
@@ -9,7 +9,7 @@ export const comments = (state = { errMess: null, comments: [] }, action) => {
       return { ...state, errMess: action.payload };
 
     case ActionTypes.ADD_COMMENT:
-      var comment = action.payload;
+      var comment = action.payload.comment;
       return { ...state, comments: state.comments.concat(comment) };
 
     default:
