0badf39a012aef88f6a3c590634324403602bd73
Meddhavi <meddhavisatija@gmail.com>
2020-06-26 18:11:55 +0530

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -13,6 +13,8 @@ import {
 import { Card } from "react-native-elements";
 import DatePicker from "react-native-datepicker";
 import * as Animatable from "react-native-animatable";
+import * as Permissions from "expo-permissions";
+import { Notifications } from "expo";
 
 class Reservation extends Component {
   constructor(props) {
@@ -30,16 +32,43 @@ class Reservation extends Component {
     title: "Reserve Table",
   };
 
-  /* toggleModal() {
-    this.setState({ showModal: !this.state.showModal });
-  }*/
-
   resetForm() {
     this.setState({
       guests: 1,
       smoking: false,
       date: "",
       showModal: false,
+    });
+  }
+
+  async obtainNotificationPermission() {
+    let permission = await Permissions.getAsync(
+      Permissions.USER_FACING_NOTIFICATIONS
+    );
+    if (permission.status !== "granted") {
+      permission = await Permissions.askAsync(
+        Permissions.USER_FACING_NOTIFICATIONS
+      );
+      if (permission.status !== "granted") {
+        Alert.alert("Permission not granted to show notifications");
+      }
+    }
+    return permission;
+  }
+
+  async presentLocalNotification(date) {
+    await this.obtainNotificationPermission();
+    Notifications.presentLocalNotificationAsync({
+      title: "Your Reservation",
+      body: "Reservation for " + date + " requested",
+      ios: {
+        sound: true,
+      },
+      android: {
+        sound: true,
+        vibrate: true,
+        color: "#512DA8",
+      },
     });
   }
 
@@ -126,6 +155,7 @@ class Reservation extends Component {
                   {
                     text: "OK",
                     onPress: () => {
+                      this.presentLocalNotification(this.state.date);
                       this.resetForm();
                     },
                   },
